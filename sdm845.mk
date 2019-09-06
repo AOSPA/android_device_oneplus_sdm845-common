34c35a11bf414f6e59b458ee04e494bb47fb50da
Chris Crump <chris@cypheros.co>
2019-09-06 17:55:09 -0400

    sdm845-common: Temporary change to build
    Change-Id: I538beb12f08be1f69507f106ba30913acbbffebf

--- sdm845.mk
+++ sdm845.mk
@@ -19,8 +19,7 @@ $(call inherit-product-if-exists, vendor/oneplus/sdm845-common/sdm845-common-ven
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-pa
+    $(LOCAL_PATH)/overlay
 
 # Properties
 -include $(LOCAL_PATH)/common-props.mk
