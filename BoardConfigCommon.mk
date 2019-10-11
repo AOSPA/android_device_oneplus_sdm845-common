2a92011438fdeac297935c7f979271eed235abb8
Jake Weinstein <jake@aospa.co>
2019-10-11 05:01:40 +0200

    sdm845-common: inherit common CAF vendor framework compatibility matrix
    Change-Id: I50b9cf14f5987558304cd734c9cafa68de94d946

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -105,7 +105,7 @@ TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 TARGET_GESTURES_NODE := "/proc/touchpanel/gesture_enable"
 
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/vendor_framework_compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := vendor/qcom/opensource/core-utils/vendor_framework_compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(COMMON_PATH)/framework_manifest.xml
 
 # Lights
