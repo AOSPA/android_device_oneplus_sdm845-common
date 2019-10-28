97c2551b6dbec189d3ff7f2e89dd86e92b87de4b
Jake Weinstein <jake@aospa.co>
2019-10-28 04:16:47 +0100

    oneplus6: enable UXE in perfconfigstore
    Matches stock Qualcomm configuration and improves performance.
    OnePlus disables this because of their framework features, which
    we do not have.
    
    Change-Id: I2b6e8c9ed59415bc7841bf244bd3dd9b5633088c

--- sdm845.mk
+++ sdm845.mk
@@ -157,4 +157,5 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_TARGET_VNDK_VERSION := 29
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/msm_irqbalance.conf
+    $(LOCAL_PATH)/configs/msm_irqbalance.conf:system/product/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/msm_irqbalance.conf \
+    $(LOCAL_PATH)/configs/perfconfigstore.xml:system/product/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/perf/perfconfigstore.xml
